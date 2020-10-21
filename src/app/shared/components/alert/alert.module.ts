7230363c2c7aadfced664edd45c62b0850774d74
Eduardo Astolfi <eastolfi91@gmail.com>
2020-10-22 00:40:05 +0200

    chore(deps): Update to Angular 10 and add firebase

--- src/app/shared/components/alert/alert.module.ts
+++ src/app/shared/components/alert/alert.module.ts
@@ -6,26 +6,26 @@ import { AlertService } from './alert.service';
 
 
 @NgModule({
-	declarations: [
-		AlertComponent
-	],
-	imports: [
-		CommonModule
-	],
-	exports: [
-		AlertComponent
-	]/*,
+    declarations: [
+        AlertComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        AlertComponent
+    ]/*,
 	providers: [
 		AlertService
 	]*/
 })
 export class AlertModule {
-	public static forRoot(): ModuleWithProviders {
-		return {
-			ngModule: AlertModule,
-			providers: [
-				AlertService
-			]
-		}
-	}
+    public static forRoot(): ModuleWithProviders<AlertModule> {
+        return {
+            ngModule: AlertModule,
+            providers: [
+                AlertService
+            ]
+        }
+    }
 }
