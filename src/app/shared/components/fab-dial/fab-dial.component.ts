dc1eeae783ac46f26176c7cb8da529c34d5b8d29
Eduardo Astolfi <eastolfi91@gmail.com>
2020-11-03 21:23:43 +0100

    fix(fab-dial) fix the position and disable the items

--- src/app/shared/components/fab-dial/fab-dial.component.ts
+++ src/app/shared/components/fab-dial/fab-dial.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { SafeResourceUrl } from '@angular/platform-browser';
 
 const ANGLE_90 = 90;
 
 export interface DialItem {
     icon: string;
+    disabled?: boolean;
     onClick: () => void;
     translateX?: number;
     translateY?: number;
@@ -19,6 +19,11 @@ export class FabDialComponent implements OnInit {
     @Input()
     public items: DialItem[];
 
+    @Input()
+    public disabled: boolean = false;
+
+    public isOpen = false;
+
     constructor() { }
 
     ngOnInit(): void {
@@ -29,47 +34,44 @@ export class FabDialComponent implements OnInit {
                 this.closeDial();
             };
         });
+    }
+
+    public openDial(): void {
+        this.isOpen = true;
         this.updateItemsPositions();
     }
 
-    // TODO - Animations
+    public closeDial(): void {
+        this.isOpen = false;
+        this.resetItemsPositions();
+    }
+
+    public toggleDial(): void {
+        if (this.isOpen) {
+            this.closeDial();
+        } else {
+            this.openDial();
+        }
+    }
+
     private updateItemsPositions() {
-        // for (let i = this.items.length - 1; i >= 0; i--) {
         for (let i = 0; i < this.items.length; i++) {
-            const offsetAngle = 360 / this.items.length;
+            const offsetAngle = 175 / this.items.length;
             const rotateAngle = offsetAngle * i;
-            // this.items[i].translateX = -5;
 
             const radius = 100;
-            const angle = (rotateAngle - 180) * (Math.PI / 180);
+            const angle = (rotateAngle - 205) * (Math.PI / 180);
             const cos = Math.cos(angle);
             const sin = Math.sin(angle);
             this.items[i].translateX = radius * cos;
             this.items[i].translateY = radius * sin;
-
-
-
-            // (x0 + r cos theta, y0 + r sin theta)
         }
     }
 
-//     var updateLayout = function(listItems) {
-//   for (var i = 0; i < listItems.length; i++) {
-//     var offsetAngle = 360 / listItems.length;
-//     var rotateAngle = offsetAngle * i;
-//     $(listItems[i]).css("transform", "rotate(" + rotateAngle + "deg) translate(0, -200px) rotate(-" + rotateAngle + "deg)")
-//   };
-// };
-
-    public isOpen = false;
-    public openDial(): void {
-        this.isOpen = true;
+    private resetItemsPositions() {
+        this.items.forEach((item: DialItem) => {
+            item.translateX = 0;
+            item.translateY = 0;
+        });
     }
-    public closeDial(): void {
-        this.isOpen = false;
-    }
-    public toggleDial(): void {
-        this.isOpen = !this.isOpen;
-    }
-
 }
